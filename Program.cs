aea70c16f812f083b1d81d4a60c88606d2fe7a4d
Maria <antonovamari94@gmail.com>
2022-11-30 14:20:10 +0700

    Начало кода4

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-string[] array1 = new string[5] {"Hi", "43", "hello", "world", "ros"};
+string[] array1 = new string[5] {"Hi", "Bay", "hello", "world", "ros"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {    int count = 0;
